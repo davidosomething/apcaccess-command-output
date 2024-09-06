6fc7798d027ed02d0a21559ebee9b1ed674a52cc
David O'Trakoun <me@davidosomething.com>
2024-09-06 12:04:25 -0400

    aaco takes optional host:port, show low battery if <90% charged

--- aaco.bash
+++ aaco.bash
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
+# param 1 "host:port"
 main() {
-  command -v apcaccess >/dev/null || return 1
-  local out
-  out="$(apcaccess)"
-  battery_percent=$(grep "BCHARGE" <<< "$out" | awk '{ print $3; }')
-  if grep -q "STATUS   : ONLINE" <<< "$out"; then
-    printf "UPS %s\n" "$battery_percent"
-  else
-    printf "UPS OFF\n"
-  fi
+	command -v apcaccess >/dev/null || return 1
+	local out
+	out="$(apcaccess -h ${1:-"localhost:3551"})"
+
+	local pct
+	pct=$(grep "BCHARGE" <<<"$out" | awk '{ print $3; }')
+
+	local threshhold=90
+	local icon
+	if (($(echo "${pct} > ${threshhold}" | bc -l))); then
+		icon="🔋"
+	else
+		icon="🪫"
+	fi
+
+	if grep -q "STATUS   : ONLINE" <<<"$out"; then
+		printf "UPS%s%s\n" "$icon" "$pct"
+	else
+		printf "UPS OFF\n"
+	fi
 }
 
-main
+main "$@"
--- upscco.bash
+++ upscco.bash
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
 main() {
-  command -v upsc >/dev/null || return 1
-  local out
-  out="$(upsc "$1")"
-  battery_percent=$(grep "battery.charge:" <<< "$out" | awk '{ print $2; }')
-  if grep -q "ups.status: OL" <<< "$out"; then
-    printf "UPS %s\n" "$battery_percent"
-  else
-    printf "UPS OFF\n"
-  fi
+	command -v upsc >/dev/null || return 1
+	local out
+	out="$(upsc "$1")"
+
+	local pct
+	pct=$(grep "battery.charge:" <<<"$out" | awk '{ print $2; }')
+
+	local threshhold=90
+	local icon
+	if (($(echo "${pct} > ${threshhold}" | bc -l))); then
+		icon="🔋"
+	else
+		icon="🪫"
+	fi
+
+	if grep -q "ups.status: OL" <<<"$out"; then
+		printf "UPS%s%s\n" "$icon" "$pct"
+	else
+		printf "UPS OFF\n"
+	fi
 }
 
 main "$1"
