13da0e9a84652dfe6aecfc571fb98ca134112b55
David O'Trakoun <me@davidosomething.com>
2024-11-22 09:53:18 -0500

    output wattage, add editorconfig

--- aaco.bash
+++ aaco.bash
@@ -4,24 +4,39 @@
 main() {
 	command -v apcaccess >/dev/null || return 1
 	local out
-	out="$(apcaccess -h ${1:-"localhost:3551"})"
+	out="$(apcaccess -h "${1:-"localhost:3551"}")"
 
-	local pct
-	pct=$(grep "BCHARGE" <<<"$out" | awk '{ print $3; }')
+	# ===========================================================================
+	# Battery charge
+	# ===========================================================================
 
-	local threshhold=90
+	local charge_threshold=90
+	local charge
+	charge=$(grep "BCHARGE" <<<"$out" | awk '{ print $3; }')
 	local icon
-	if (($(echo "${pct} > ${threshhold}" | bc -l))); then
+	if (($(echo "${charge} > ${charge_threshold}" | bc -l))); then
 		icon="🔋"
 	else
 		icon="🪫"
 	fi
-
 	if grep -q "STATUS   : ONLINE" <<<"$out"; then
-		printf "UPS%s%s\n" "$icon" "$pct"
+		printf "UPS%s%s" "$icon" "$charge"
 	else
-		printf "UPS OFF\n"
+		printf "UPS OFF"
 	fi
+
+	# ===========================================================================
+	# Power consumption
+	# ===========================================================================
+
+	local loadpct
+	loadpct=$(grep "LOADPCT" <<<"$out" | awk '{ print $3; }')
+	local nompower
+	nompower=$(grep "NOMPOWER" <<<"$out" | awk '{ print $3; }')
+	local loadw
+	loadw=$(bc -l <<<"${nompower} * (${loadpct} / 100)")
+	# display only whole number
+	printf " | %s W\n" "${loadw%.*}"
 }
 
 main "$@"
